87689e0d3dc8cdf23a278e9b7b40aeef8a9cacb6
oscar <os.amilcar@hotmail.com>
2020-06-25 11:29:34 -0600

    creado componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListaClientesComponent } from './cliente/lista-clientes/lista-clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListaClientesComponent
   ],
   imports: [
     BrowserModule,
