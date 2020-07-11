5fbb4cfe4a16be41955515109519774cf510f3d4
oscar <os.amilcar@hotmail.com>
2020-07-11 13:01:03 -0600

    modulo compartido

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports:[
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatFormFieldModule
   ]
 })
 export class SharedModule { }
